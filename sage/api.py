277413fedd466c3a7623038098a74aefd4544618
Namith Chelikani <chelikaninamith@gmail.com>
2025-03-27 09:37:17 -0500

    stuff

--- sage/api.py
+++ sage/api.py
@@ -1,3 +1,37 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+import datetime
+
+def get_price(ticker):
+    try:
+        time.sleep(10)
+        url = f'https://finance.yahoo.com/quote/{ticker}/'
+        # apparently these headers make me seem like a human lol
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+        response = requests.get(url, headers=headers)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        price_element = soup.find(attrs={"data-testid": "qsp-price"})
+
+        if price_element:
+            return float(price_element.text.replace(",","").strip())
+        else:
+            return "failed"
+    except:
+        time.sleep(30)
+        url = f'https://finance.yahoo.com/quote/{ticker}/'
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+        response = requests.get(url, headers=headers)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        price_element = soup.find(attrs={"data-testid": "qsp-price"})
+
+        if price_element:
+            return float(price_element.text.replace(",","").strip())
+        else:
+            return "failed"
+
 def get_market_cap(ticker):
     try:
         time.sleep(10)
@@ -56,3 +90,42 @@ def get_market_cap(ticker):
                 actual = price_element.text.replace(",","").strip()
         return float(actual)
 
+def get_market_open(ticker):
+    try:
+        time.sleep(10)
+        url = f'https://finance.yahoo.com/quote/{ticker}/'
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+        response = requests.get(url, headers=headers)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        opens = soup.find(attrs={"data-field": "regularMarketOpen"})
+
+        if not opens:
+            return "failed"
+
+        opens = opens.text
+
+
+        return float(opens.replace(",","").strip())
+    except:
+        time.sleep(30)
+        url = f'https://finance.yahoo.com/quote/{ticker}/'
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+        response = requests.get(url, headers=headers)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        opens = soup.find(attrs={"data-field": "regularMarketOpen"})
+
+        if not opens:
+            return "failed"
+
+        opens = opens.text
+
+
+        return float(opens.replace(",","").strip())
+
+def get_time():
+    now = datetime.datetime.now()
+    midnight = now.replace(hour=0, minute=0, second=0, microsecond=0)
+    minutes_since_midnight = (now - midnight).seconds // 60
+    return int(minutes_since_midnight)
